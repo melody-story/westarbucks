dcc651948f8308d9bba15722a5395b6e26f0a97c
Melody's lap top <melody@Melodysui-MacBookPro.local>
2021-08-17 00:28:31 +0900

    first commit

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '24_westarbucks',
+        'USER': 'root',
+        'PASSWORD': 'Melody9653!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = '쉿'
---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+
+class Menu(models.Model):
+	name = models.CharField(max_length=20)
+
+	class Meta:
+         db_table = 'menus'
+
+class Category(models.Model):
+	name = models.CharField(max_length=20)
+	menu = models.ForeignKey('Menu', on_delete=models.CASCADE)
+
+	class Meta:
+    		db_table = 'categories'
+
+class Product(models.Model):
+	name     = models.CharField(max_length=100)
+	category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+	class Meta:
+        	db_table = 'products'
+
+
+# Create your models here.
